b4ecfa2c5832b4d6879b81d26af7e86d77d12a71
USER <USER@172.16.220.201>
2022-04-27 08:24:55 -0500

    CLASE 27/04/2022

---
+++ src/main/java/servicio/AutoServicio.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Auto;
+
+/**
+ *
+ * @author morti
+ */
+public class AutoServicio implements IAutoServicio{
+    
+    private final List<Auto> autoList = new ArrayList<>();
+     
+
+    public Auto crear(Auto auto){
+        this.autoList.add(auto);
+        return auto;
+    }
+    
+    public List<Auto> listar(){   
+        return this.autoList;
+    }   
+    
+}
